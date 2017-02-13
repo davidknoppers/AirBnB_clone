4500cd3fd68db4384f6b1984bd34859c834383c5
davidknoppers <david.knoppers@holbertonschool.com>
2017-02-13 07:41:49 +0000

    adding docs

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,18 @@
 import uuid
 import models
 from datetime import datetime, date, time
+"""
+Base model for all other classes
+Sets up attributes for other classes to inherit
+Features basic information
+"""
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        """
+        creates a basemodel using setattr
+        """
         if len(args) > 0:
             for i in args[0]:
                 setattr(self, i, args[0][i])
@@ -16,21 +24,36 @@ class BaseModel:
             models.storage.save()
 
     def my_number(self, my_number):
+        """
+        simple setter for number
+        """
         self.my_number = my_number
 
     def name(self, name):
+        """
+        simple setter for name
+        """
         self.name = name
 
     def save(self):
+        """
+        calls filestorage to save stuff
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def __str__(self):
+        """
+        returns string rep of an object
+        """
         return "[{}] ({}) {}".format(type(self).
                                      __name__, self.id, self.__dict__)
 
     def to_json(self):
-        """Shout out to Danton"""
+        """
+        Shout out to Danton for the help
+        converts objects to json for storage
+        """
         temp = {}
         for i in self.__dict__.keys():
             if (isinstance(self.__dict__[i], datetime)):
