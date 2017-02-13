d1a964c0d9f1530775f57624d09291353088c8ae
davidknoppers <david.knoppers@holbertonschool.com>
2017-02-13 07:37:49 +0000

    fixed reload!

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,40 @@
 #!/usr/bin/python3
+"""
+Filestorage contains all the functionality for file i/o
+This functionality could be in base_model but we've split it up for learning
+Reload is currently broken :((
+"""
 import json
 import os
+from datetime import *
 from models import *
 
 
 class FileStorage:
+    """
+    saves, prints, reloads, and makes new objects
+    should work for all calls, direct or by object
+    employs json serialization/deserialization
+    """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
+        """
+        print all objects
+        """
         return FileStorage.__objects
 
     def new(self, obj):
+        """
+        make a new object
+        """
         FileStorage.__objects[obj.id] = obj
 
     def save(self):
+        """
+        save an instance
+        """
         serialized = {}
         for key in FileStorage.__objects.keys():
             serialized[key] = FileStorage.__objects[key].to_json()
@@ -22,6 +42,9 @@ class FileStorage:
             MyFile.write(json.dumps(serialized))
 
     def reload(self):
+        """
+        load the json file, in theory
+        """
         try:
             with open(FileStorage.__file_path,
                       mode="r+", encoding="utf-8") as MyFile:
@@ -33,6 +56,7 @@ class FileStorage:
                     cr_at = datetime.strptime(cr_at, "%Y-%m-%d %H:%M:%S.%f")
                     up_at = temp[i]["updated_at"]
                     up_at = datetime.strptime(up_at, "%Y-%m-%d %H:%M:%S.%f")
-                    FileStorage.__objects[k] = eval(class_)(temp[i])
+                    FileStorage.__objects[i] = eval(class_)(temp[i])
         except Exception as e:
+            print(e)
             pass
