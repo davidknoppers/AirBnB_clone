1445ba41e72eb9ec799e63641bd5ffd185383a88
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-15 19:27:06 +0000

    added update and sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
@@ -8,5 +9,5 @@ sudo mkdir -p /data/web_static/releases/test/
 echo 'julienspecial' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo sed -i "39i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
