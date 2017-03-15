1df356177edd747e3e4e3bc0e3d535c9e08c9de7
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-15 21:54:39 +0000

    removed slash in mkdir

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import *
 def do_pack():
     """ tarballs web_static"""
     try:
-        local("sudo mkdir -p versions/")
+        local("sudo mkdir -p versions")
         local("sudo tar -zcvf \"./versions/web_st\
         atic_`date +%Y%m%d%H%M%S`.tgz\"web_static")
     except:
