61f90577af9dae61391826601466c977b429329d
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-15 21:22:58 +0000

    untested draft, though Jennie looked at it so that sort of counts

---
+++ 1-pack_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# makes a tarball by using fabric local to call shell commands
+from fabric.api import *
+
+
+def do_pack():
+    """ tarballs web_static"""
+    try:
+        local("sudo mkdir -p versions/")
+        local("sudo tar -cvzf \
+        \"./versions/web_static_`date+%Y%m%d%H%M%S`.tgz \"web_static")
+    except:
+        return
