232e80d9f4e17bbb3d736fbce9edee5a65c483d3
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-15 23:42:40 +0000

    untested draft

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# distributes an archive to our web servers
+from fabric.api import *
+env.hosts = ['52.90.159.84', '54.242.30.104']
+
+
+def do_deploy(archive_path):
+    """ deploy that archive"""
+    #get path to web server tmp
+    try:
+        put(archive_path, "/tmp/")
+        path = archive_path.strip("versions/")
+        directory = "/data/web_static/releases/"
+        temp = "sudo mkdir -p " + directory + path
+        run(temp)
+        temp = "sudo tar -xzf /tmp/" + path + " -C " + directory + path
+        run(temp)
+        run("rm /tmp/" + path)
+        temp = "sudo mv " + directory
+        temp += "/web_static/* " + directory + path + "/"
+        run(temp)
+        temp = "sudo rm -rf " + directory + path + "/web_static"
+        run(temp)
+        temp = "rm -rf " + directory
+        run(temp)
+        temp = "sudo ln -s " + directory + path + "/ /data/web_static/current"
+        run(temp)
+        return True
+    except:
+        return False
