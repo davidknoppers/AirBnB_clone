7e581f7d2ec9b78aaa88a9e3d7db8c7975bd9582
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-16 03:44:39 +0000

    not sure how to access path without global variable

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+distributes an archive to our web servers
+we are learning how to properly manage code deployment
+with tar and fabric
+"""
+from fabric.api import *
+import sys
+import time
+
+
+env.hosts = ['52.90.159.84', '54.242.30.104']
+archive_path = None
+
+
+def do_pack():
+    """ tarballs web_static"""
+    path = "web_static_{:s}.tgz".format(time.strftime("%Y%m%d%H%M%S"))
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/{} web_static/".format(path))
+        return("versions/{}".format(path))
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    uploads tarball
+    unpacks tarball
+    cleans up directories
+    """
+    try:
+        put(archive_path, "/tmp/")
+        path = archive_path.strip("versions/")
+        directory = "/data/web_static/releases/"
+        temp = "sudo mkdir -p " + directory + path
+        run(temp)
+        temp = "sudo tar -xzf /tmp/" + path + " -C " + directory + path
+        run(temp)
+        temp = "sudo rm /tmp/" + path
+        run(temp)
+        temp = "sudo mv " + directory + path
+        temp += "/web_static/* " + directory + path + "/"
+        run(temp)
+        temp = "sudo rm -rf " + directory + path + "/web_static"
+        run(temp)
+        temp = "sudo rm -rf /data/web_static/current"
+        run(temp)
+        temp = "sudo ln -s " + directory + path + "/ /data/web_static/current"
+        run(temp)
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """
+    put do_pack and do_deploy together
+    packs the code, deploys the code to two servers
+    cleans the servers
+    """
+    global archive_path
+    if not archive_path:
+        archive_path = do_pack()
+    if not archive_path:
+        return False
+    path = path.strip('\n')
+    temp = do_deploy(path)
+    return (temp)
