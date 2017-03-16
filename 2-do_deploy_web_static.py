edb4b4a3d7ccf2801f5b6bd52f6f9c5926898f16
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-16 02:57:23 +0000

    including file 1 for use in file 3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
-""" distributes an archive to our web servers """
+"""
+distributes an archive to our web servers
+we are learning how to properly manage code deployment
+with tar and fabric
+"""
 from fabric.api import *
+import sys
+import time
 
 
 env.hosts = ['52.90.159.84', '54.242.30.104']
 
 
+def do_pack():
+    """ tarballs web_static"""
+    path = "web_static_{:s}.tgz".format(time.strftime("%Y%m%d%H%M%S"))
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/{} web_static/".format(path))
+    except:
+        return None
+
+
 def do_deploy(archive_path):
-    """ upload archive and delete from web server"""
+    """
+    uploads tarball
+    unpacks tarball
+    cleans up directories
+    """
     try:
         put(archive_path, "/tmp/")
         path = archive_path.strip("versions/")
